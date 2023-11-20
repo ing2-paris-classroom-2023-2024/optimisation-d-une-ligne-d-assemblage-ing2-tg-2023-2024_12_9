0233904d70a2e56732139a39e93dcf75f9882fa4
Julien Hogommat <you@example.com>
2023-11-20 10:07:02 +0100

    hello

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/audri/CLionProjects/optimisation-d-une-ligne-d-assemblage-td12_grp9
+# Install script for directory: C:/Users/jhogo/CLionProjects/optimisation-d-une-ligne-d-assemblage-td12_grp9
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/MinGW/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Users/jhogo/Downloads/MinGW avec Allegro 4/MinGW/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/audri/CLionProjects/optimisation-d-une-ligne-d-assemblage-td12_grp9/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/jhogo/CLionProjects/optimisation-d-une-ligne-d-assemblage-td12_grp9/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
