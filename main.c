c053a2e5674be93ca0e92c8fd479ad1d2eeb5b87
Aupiet <audric91300@gmail.com>
2023-12-03 17:27:30 +0100

    -mise en place du menu avec les contraintes de temps et de précédence

--- main.c
+++ main.c
@@ -13,52 +13,76 @@ int main()
     tab.tableauPrecedences = NULL;
     tab.nombreMachines = 0;
     tab.nombreTaches = 0;
-    precedences(&tab);
-    printf("Nombre de machines : %d\n", tab.nombreMachines);
-    printf("Nombre de tâches : %d\n", tab.nombreTaches);
-    // realloc le tableau de précédences au nombre de machines
-    tab.tableauPrecedences = realloc(tab.tableauPrecedences, tab.nombreMachines * sizeof(int*));
-    // compter le nombre de tâches
-    tab.nombreTaches = 0;
-    for(int i = 0; i < tab.nombreMachines; i++){
-        int compteur = 0;
-        while(tab.tableauPrecedences[i][compteur] != 0){
-            compteur++;
-        }
-        if (compteur > tab.nombreTaches)tab.nombreTaches = compteur;
-    }
-    for ( int i = 0; i < tab.nombreMachines; i++){
-        tab.tableauPrecedences[i] = realloc(tab.tableauPrecedences[i], tab.nombreTaches * sizeof(int));
-    }
-    printf("Nombre de tâches : %d\n", tab.nombreTaches);
-    // afficher le tableau de précédences
-    for(int i = 0; i < tab.nombreMachines; i++){
-        printf("Machine %d : ", i);
-        for(int j = 0; j < tab.nombreTaches; j++){
-            printf("%d ", tab.tableauPrecedences[i][j]);
-        }
-        printf("\n");
-    }
-    initilisationtempscycle(&tab);
-    // afficher le tableau de précédences
-    for(int i = 0; i < tab.nombreMachines; i++){
-        printf("Machine %d : ", i);
-        for(int j = 0; j < tab.nombreTaches; j++){
-            if (tab.tableauPrecedences[i][j] != 0) printf("%d ", tab.tableauPrecedences[i][j]);
-        }
-        printf("\n");
-    }
-    /*
+
+
     int choix;
     do {
         choix = menu_de_lancement();
     } while (choix == 0);
-    return 0;*/
-
+    if (choix == 1) {
+        /*
+        exclusion(&tab);
+        // afficher le tableau de précédences
+        for(int i = 0; i < tab.nombreMachines; i++){
+            printf("Machine %d : ", i);
+            for(int j = 0; j < tab.nombreTaches; j++){
+                if (tab.tableauPrecedences[i][j] != 0) printf("%d ", tab.tableauPrecedences[i][j]);
+            }
+            printf("\n");
+        }*/
+    }
+    if (choix == 2) {
+        /*
+        temps_de_cycle(&tab);
+        // afficher le tableau de précédences
+        for(int i = 0; i < tab.nombreMachines; i++){
+            printf("Machine %d : ", i);
+            for(int j = 0; j < tab.nombreTaches; j++){
+                if (tab.tableauPrecedences[i][j] != 0) printf("%d ", tab.tableauPrecedences[i][j]);
+            }
+            printf("\n");
+        }*/
+    }
+    if (choix == 3) {
+        precedences(&tab);
+        // realloc le tableau de précédences au nombre de machines
+        tab.tableauPrecedences = realloc(tab.tableauPrecedences, tab.nombreMachines * sizeof(int*));
+        // compter le nombre de tâches
+        tab.nombreTaches = 0;
+        for(int i = 0; i < tab.nombreMachines; i++){
+            int compteur = 0;
+            while(tab.tableauPrecedences[i][compteur] != 0){
+                compteur++;
+            }
+            if (compteur > tab.nombreTaches)tab.nombreTaches = compteur;
+        }
+        for ( int i = 0; i < tab.nombreMachines; i++){
+            tab.tableauPrecedences[i] = realloc(tab.tableauPrecedences[i], tab.nombreTaches * sizeof(int));
+        }
+        printf("Nombre de tâches : %d\n", tab.nombreTaches);
+        // afficher le tableau de précédences
+        for(int i = 0; i < tab.nombreMachines; i++){
+            printf("Machine %d : ", i);
+            for(int j = 0; j < tab.nombreTaches; j++){
+                printf("%d ", tab.tableauPrecedences[i][j]);
+            }
+            printf("\n");
+        }
+        initilisationtempscycle(&tab);
+        // afficher le tableau de précédences
+        for(int i = 0; i < tab.nombreMachines; i++){
+            printf("Machine %d : ", i);
+            for(int j = 0; j < tab.nombreTaches; j++){
+                if (tab.tableauPrecedences[i][j] != 0) printf("%d ", tab.tableauPrecedences[i][j]);
+            }
+            printf("\n");
+        }
+    }
     // libération de la mémoire
     for (int i = 0; i < tab.nombreMachines; ++i)
     {
         free(tab.tableauPrecedences[i]);
     }
     free(tab.tableauPrecedences);
+    return 0;
 }
